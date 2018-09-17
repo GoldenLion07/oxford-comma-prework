84e618e5ca41280512b6e9423e1735179da15b94
Scott M <gmat191@gmail.com>
2018-09-17 07:18:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 1 
     return "#{array[0]}"
-    elsif array.lenght == 2 
+    elsif array.length == 2 
     return array.join(' and ')
     array[-1] = "and #{array[-1]}"
     return array.join(", ")
